e3927342c98661a08b980758d243bf2e60ddfb86
joSincal <jguitzol@universales.com>
2022-07-19 16:56:33 -0600

    Build de la aplicacion

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -6,19 +6,11 @@ import { Personaje } from '../interfaces/dbz.interface';
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000,
   };
+
+  constructor() {}
 }
