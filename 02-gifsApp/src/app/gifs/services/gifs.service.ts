378d5611782cfea8627ce81ed67643ed702f8617
jSincal <ottozincal@gmail.com>
2022-07-20 13:04:42 -0600

    tipado a peticiones http

---
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _apiKey: string = 'rV8V3hfprM1cnWNQHBQb5jzuER10SNXp';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {}
+
+  buscarGifs(query: string = '') {
+    query = query.trim().toLocaleLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+
+    this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this._apiKey}&q=${query}&limit=10`
+      )
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
+  }
+}
