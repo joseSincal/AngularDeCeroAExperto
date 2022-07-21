93351ef29419a5b571b405ffd995fd8d6c478e0b
jSincal <ottozincal@gmail.com>
2022-07-21 17:47:27 -0600

    Servicio para buscar paises

--- 03-paisesApp/src/app/shared/shared.module.ts
+++ 03-paisesApp/src/app/shared/shared.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
-
 @NgModule({
-  declarations: [
-    SidebarComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    SidebarComponent
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [SidebarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
--- 03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
+++ 03-paisesApp/src/app/shared/sidebar/sidebar.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class SidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
