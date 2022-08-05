ff467465bd721767ada5e76e584448145c79b0d2
jSincal <ottozincal@gmail.com>
2022-08-05 16:54:36 -0600

    Formulario de crear

--- 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_image: ''
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
