e18012d360f670786815fda13ff53e92d16cda31
joSincal <jguitzol@universales.com>
2022-08-30 16:58:23 -0600

    Menu de aplicacion - Mapas

--- 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
+++ 06-formulariosApp/src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
+
+/** 
+import { nombreApellidoPattern, emailPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
+*/
+import { ValidatorService } from 'src/app/shared/validator/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,19 +13,67 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [],
 })
 export class RegistroComponent implements OnInit {
-  // TODO: Temporal
-  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  miFormulario: FormGroup = this.fb.group(
+    {
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.nombreApellidoPattern),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorService.noPuedeSerStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.validatorService.camposIguales('password', 'password2'),
+      ],
+    }
+  );
 
-  miFormulario: FormGroup = this.fb.group({
-    nombre: [
-      '',
-      [Validators.required, Validators.pattern(this.nombreApellidoPattern)],
-    ],
-  });
+  get emailErrorMsg(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+    if (errors?.['required']) {
+      return 'Email es obligatorio';
+    }
+    else if (errors?.['pattern']) {
+      return 'El valor ingresado no tiene formato de correo';
+    }
+    else if (errors?.['emailTomado']) {
+      return 'El email ya esta en uso';
+    }
 
-  constructor(private fb: FormBuilder) {}
+    return '';
+  }
 
-  ngOnInit(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorService,
+    private emailValidator: EmailValidatorService
+  ) {}
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'Jose Sincal',
+      email: 'test1@test.com',
+      username: 'js2',
+      password: '123456',
+      password2: '123456',
+    });
+  }
 
   campoNoValido(campo: string) {
     return (
---
+++ 09-mapasApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoiam9zc2l0bzAyIiwiYSI6ImNsN2dyaWt1MzAxNWIzbm85cGdyYWcxNXUifQ.qG7qQXUItlHE87-bPxiuXA'
+};
