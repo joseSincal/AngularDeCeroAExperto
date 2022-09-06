d3539f401e5417336111dc9c44faa6f6868cc337
joSincal <jguitzol@universales.com>
2022-09-06 11:55:07 -0600

    Pagina zoom range

--- 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
+++ 09-mapasApp/src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
   styles: [
-  ]
+    `
+      #mapa {
+        height: 100%;
+        width: 100%;
+      }
+    `,
+  ],
 })
 export class FullScreenComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-90.5165653955135, 14.613149519206319],
+      zoom: 15
+    });
   }
-
 }
--- 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 09-mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        height: 100%;
+        width: 100%;
+      }
+
+      .row {
+        background-color: white;
+        border-radius: 5px;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        z-index: 999;
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-90.5165653955135, 14.613149519206319],
+      zoom: 15,
+    });
   }
-
 }
