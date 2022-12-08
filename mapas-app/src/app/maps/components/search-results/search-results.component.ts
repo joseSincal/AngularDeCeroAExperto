e6d46abc2b6bce8d53eae9fa66c69bcfa93d3f6f
jSincal <ottozincal@gmail.com>
2022-12-08 17:21:57 -0600

    Final de curso Angular de 0 a experto

--- mapas-app/src/app/maps/components/search-results/search-results.component.ts
+++ mapas-app/src/app/maps/components/search-results/search-results.component.ts
@@ -25,8 +25,19 @@ export class SearchResultsComponent {
 
   flyTo(place: Feature) {
     this.selectedId = place.id;
-    
+
     const [lng, lat] = place.center;
     this.mapService.flyTo([lng, lat]);
+  }
+
+  getDirections(place: Feature) {
+    if (!this.placesService.userLocation) throw Error('No hay userLocation');
+
+    this.placesService.deletePlaces();
+
+    const start = this.placesService.userLocation;
+    const end = place.center as [number, number];
+
+    this.mapService.getRouteBetweenPoints(start, end);
   }
 }
