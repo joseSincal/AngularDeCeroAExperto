c4cac2a55e50b1d9c150045edf0e35ebac80a09b
joSincal <jguitzol@universales.com>
2023-02-22 10:23:35 -0600

    final commit

--- 12-auth-server/index.js
+++ 12-auth-server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const { dbConnection } = require("./db/config");
 require("dotenv").config();
 
@@ -20,6 +22,11 @@ app.use(express.json());
 
 // RUTAS
 app.use("/api/auth", require("./routes/auth"));
+
+// MANEJAR DEMAS RUTAS
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puesto ${process.env.PORT}`);
