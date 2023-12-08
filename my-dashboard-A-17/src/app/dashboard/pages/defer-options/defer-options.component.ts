268bc06584fb6bb35c56984f91e74987aa783bcf
joseSincal <106826998+joSincal@users.noreply.github.com>
2023-12-08 11:47:36 -0600

    view-transition

--- my-dashboard-A-17/src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ my-dashboard-A-17/src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options.component.html',
-  styles: ``
+  styles: ``,
 })
 export default class DeferOptionsComponent {
-
 }
